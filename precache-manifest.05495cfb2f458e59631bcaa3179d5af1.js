8cb418b8947c850572f80ef455967ef1aa7e4662
chiranz <chiranpoudyal@gmail.com>
2020-04-19 12:19:51 +0530

    Updates

---
+++ precache-manifest.05495cfb2f458e59631bcaa3179d5af1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43425c0b296314b361a5062723532310",
+    "url": "/todoist/index.html"
+  },
+  {
+    "revision": "813777c1201e2f5e3617",
+    "url": "/todoist/static/css/main.c058007a.chunk.css"
+  },
+  {
+    "revision": "802f14214a7325a46de4",
+    "url": "/todoist/static/js/2.f7fbe6b5.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/todoist/static/js/2.f7fbe6b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "813777c1201e2f5e3617",
+    "url": "/todoist/static/js/main.2ddfecda.chunk.js"
+  },
+  {
+    "revision": "42cbe74643c1a5a118dd",
+    "url": "/todoist/static/js/runtime-main.8d1c3457.js"
+  },
+  {
+    "revision": "0a0b9c0aa00ac91abfd50e7b9ee8bfaf",
+    "url": "/todoist/static/media/landing.0a0b9c0a.png"
+  },
+  {
+    "revision": "72927dc1ebd94a0c10e576e59601de97",
+    "url": "/todoist/static/media/todoist.72927dc1.png"
+  }
+]);
